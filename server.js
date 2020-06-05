4189933410b88a2a1dad695f88e2b5734cf0b131
Cauany Souza <souza.cauany99@gmail.com>
2020-06-04 23:35:24 -0300

    template engine configurada

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
 const api = express()
 
 api.use(express.static('public'))
 
-
-api.get('/', (req, res) => {
-    res.sendFile(__dirname + '/src/index.html')
+nunjucks.configure('src', {
+    express: api,
+    noCache: true
 })
 
+api.get('/', (req, res) => {
+    res.render('index.html')
+})
+
+api.get('/create-point', (req,res) => {
+    res.render('create-point.html')
+})
+
+api.get('/search', (req,res) => {
+    res.render('search-results.html')
+})
 
 api.listen('3000', (req, res) =>{
     console.log('It is running!')
